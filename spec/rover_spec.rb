6909b1b82817136c9768fd155e40c4db4a9f92a5
Dzmitriy Kalhanau <96485499+Kamon1996@users.noreply.github.com>
2022-06-05 00:39:25 +0300

    final custom

--- spec/rover_spec.rb
+++ spec/rover_spec.rb
@@ -3,13 +3,12 @@
 require 'spec_helper'
 
 describe Rover do
-  describe '#print_info' do
+  describe '#execute_rover_commands' do
     subject do
       Rover.new('5 5', '1 1 N', 'LLRRLLRR').execute_rover_commands
     end
 
-    it 'returns correct position' do
-      expect(subject).to eq([1, 1, 'N'])
+    it 'returns correct message' do
     end
   end
 end
