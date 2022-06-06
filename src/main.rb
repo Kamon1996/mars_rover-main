0088627112283b58bfe907309817d32bdc93035b
Dmitriy Kalganov <dmitriu355@gmail.com>
2022-06-06 10:58:57 +0300

    fixed

--- src/main.rb
+++ src/main.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 require_relative './rover'
+require_relative 'plateau'
 
-plateau = ARGF.gets.chomp
+plateau = Plateau.new(ARGF.gets.chomp).get
 start_position = ARGF.gets.chomp
 rover_commands = ARGF.gets.chomp
 Rover.new(plateau, start_position, rover_commands).execute_rover_commands
---
+++ src/plateau.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# Rover's Actions
+class Plateau
+  def initialize(plateau)
+    @plateau = plateau
+  end
+
+  def get
+    @plateau
+  end
+end
